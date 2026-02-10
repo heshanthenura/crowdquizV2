1668f4dd35621dedeca3ba93db0321d8090f1702
Heshan Thenura Kariyawasam <heshanthenura@protonmail.com>
2026-02-11 02:44:49 +0530

    fix: deployment issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout({
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7321285556879956"
-          crossorigin="anonymous"
         ></script>
       </head>
       <body
